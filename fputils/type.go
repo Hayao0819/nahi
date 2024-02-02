2635e5be1f07a8d6e6005af9b70a73eb5177a844
hayao <hayao@fascode.net>
2024-02-02 11:10:32 +0900

    Add: fputils.DetectFileType

---
+++ fputils/type.go
@@ -0,0 +1,14 @@
+package fputils
+
+import (
+	"net/http"
+	"os"
+)
+
+func DetectFileType(path string) (string, error) {
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		return "application/octet-stream", err
+	}
+	return http.DetectContentType(bytes), nil
+}
